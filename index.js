640313259c43445f442702acb06f749751ea3924
Abhishek A M <abhishekmagadum3@gmail.com>
2025-04-05 12:08:29 +0530

    githubdata added

--- index.js
+++ index.js
@@ -4,11 +4,21 @@ const express = require('express')
 
 const app = express()
 
+const githubdata = 
+    {
+        "message": "Not Found",
+        "documentation_url": "https://docs.github.com/rest",
+        "status": "404"
+      }
+
+
+
 const port = 4000
 
 app.get('/', (req, res) => {
   res.send('Hello World! i am abhi')
 })
+
 
 app.get('/twiter', (req,res) =>{
     res.send('google in ')
@@ -16,6 +26,10 @@ app.get('/twiter', (req,res) =>{
 
 app.get('/login',(req,res) =>{
     res.send('abhi is logged in ')
+})
+
+app.get('/githubdata',(req,res) =>{
+    res.json(githubdata)
 })
 
 app.get('/youtube',(req,res) =>{
